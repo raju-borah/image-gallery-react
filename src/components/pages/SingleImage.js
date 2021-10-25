9cfafa5620596352fd815b02b568b0ea91163ed8
Raju Moni Borah <48676590+raju-borah@users.noreply.github.com>
2021-10-25 12:52:05 +0530

    Update SingleImage.js

--- src/components/pages/SingleImage.js
+++ src/components/pages/SingleImage.js
@@ -6,7 +6,7 @@ const SimgleImage = ({ image }) => {
   const [img, setImg] = useState("");
   const searchHandlder = async () => {
     const response = await axios.get(
-      `https://pixabay.com/api/?key=23662182-a70494797e8140caffbf82463&id=${id}&image_type=photo&pretty=true`
+      `https://pixabay.com/api/?key=_&id=${id}&image_type=photo&pretty=true`
     );
     setImg(response.data.hits[0]);
   };
